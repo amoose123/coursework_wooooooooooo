999230a711a7db536c83a180967bdea3e6ee8948
amoose123 <152979277+amoose123@users.noreply.github.com>
2024-07-04 08:59:13 +0000

    start

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#appa
+# importing the library
+import pygame
+
+# initializing all the imported
+# pygame modules
+(numpass,numfail) = pygame.init()
+
+# printing the number of modules 
+# initialized successfully
+print('Number of modules initialized successfully:',
+	numpass)
